e909d4da86a67a5dab0bc11308d648abdce858e3
f-mehdibayli <farid.mehdibayli@gmail.com>
2025-06-10 03:07:12 +0400

    Main updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
 function Main({ data }) {
 
   return (
-    <main className='min-h-[80vh]'>
-      <section className="bg-[#f1f3f7] border-t border-y-solid border-t-[#eaebf2] border-b-[#eaebf2] ">
+    <main className='w-screen min-h-[80vh]'>
+      <section className="min-w-[1010px] bg-[#f1f3f7] border-t border-y-solid border-t-[#eaebf2] border-b-[#eaebf2] ">
         <div className="w-[1010px] m-auto px-[19px] py-[22px] font-[700] uppercase">
           Premium elanlar
         </div>
